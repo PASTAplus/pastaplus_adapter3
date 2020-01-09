381ff671b301519232099be227bb07a1c2be0d06
Mark Servilla <mark.servilla@gmail.com>
2020-01-09 10:36:51 -0700

    Fix multiple issues:
    1. Correct for naive datetime - apply timezones for correct querying
    2. Create datetime object from property.FROM_DATE in bootstrap for initial comparison
    3. Move local timestamp management into main() from parse()

--- src/poll_manager.py
+++ src/poll_manager.py
@@ -17,6 +17,8 @@ from datetime import datetime
 from datetime import timedelta
 import xml.etree.ElementTree as ET
 
+import pendulum
+
 from adapter_exceptions import AdapterRequestFailureException
 import adapter_utilities
 from event import Event
@@ -32,9 +34,8 @@ logger = logging.getLogger('poll_manager')
 
 def bootstrap(url=None):
     now = datetime.now()
-    fromDate = properties.FROM_DATE
-    # when bootstrapping, we don't use the last query date
-    adapter_utilities.clear_last_query_date()
+    fromDate = datetime.strptime(properties.FROM_DATE,
+                                 '%Y-%m-%dT%H:%M:%S.%f')
 
     while fromDate < now:
         toDate = fromDate + timedelta(minutes=30)
@@ -61,60 +62,51 @@ def parse(url=None, fromDate=None, toDate=None, scope=None):
     fromDate = datetime.strftime(fromDate, '%Y-%m-%dT%H:%M:%S.%f')
     if toDate is not None:
         toDate = datetime.strftime(toDate, '%Y-%m-%dT%H:%M:%S.%f')
+
     # add date(s) to url
     if fromDate is not None:
-        url = url + 'fromDate=' + fromDate + '&'
+        url = url + 'fromDate=' + fromDate
     if toDate is not None:
-        url = url + 'toDate=' + toDate + '&'
+        url = url + '&toDate=' + toDate
     if scope is not None:
-        url = url + 'scope=' + scope
+        url = url + '&scope=' + scope
 
-    # capture the current datetime for saving as last_query_date
-    query_date = datetime.now()
+    logger.info('requests_get_url_wrapper: ' + url)
+    r = adapter_utilities.requests_get_url_wrapper(url=url, rethrow=True)
 
-    try:
-        logger.info('requests_get_url_wrapper: ' + url)
-        r = adapter_utilities.requests_get_url_wrapper(url=url, rethrow=True)
+    if r is not None:
+        qm = QueueManager()
+        tree = ET.ElementTree(ET.fromstring(r.text.strip()))
+        for dataPackage in tree.iter('dataPackage'):
+            package = dataPackage.find('./packageId')
+            date = dataPackage.find('./date')
+            method = dataPackage.find('./serviceMethod')
+            owner = dataPackage.find('./principal')
+            doi = dataPackage.find('./doi')
 
-        if r is not None:
-            qm = QueueManager()
-            tree = ET.ElementTree(ET.fromstring(r.text.strip()))
-            for dataPackage in tree.iter('dataPackage'):
-                package = dataPackage.find('./packageId')
-                date = dataPackage.find('./date')
-                method = dataPackage.find('./serviceMethod')
-                owner = dataPackage.find('./principal')
-                doi = dataPackage.find('./doi')
+            event = Event()
+            event.package = package.text
+            event.datetime = date.text
+            event.method = method.text
+            event.owner = owner.text
+            event.doi = doi.text
 
-                event = Event()
-                event.package = package.text
-                event.datetime = date.text
-                event.method = method.text
-                event.owner = owner.text
-                event.doi = doi.text
-
-                # Skip fromDate record(s) that already exist in queue
-                if fromDate.rstrip('0') == date.text:
-                    msg = 'Skipping: {} - {} - {}'.format(package.text, date.text,
-                                                          method.text)
+            # Skip fromDate record(s) that already exist in queue
+            if fromDate.rstrip('0') == date.text:
+                msg = 'Skipping: {} - {} - {}'.format(package.text, date.text,
+                                                      method.text)
+                logger.warning(msg)
+            else:
+                # Provide additional filter for multiple scope values
+                package_scope = event.package.split('.')[0]
+                if package_scope in properties.PASTA_WHITELIST:
+                    msg = 'Enqueue: {} - {} - {}'.format(package.text,
+                                                         date.text, method.text)
                     logger.warning(msg)
+                    qm.enqueue(event=event)
                 else:
-                    # Provide additional filter for multiple scope values
-                    package_scope = event.package.split('.')[0]
-                    if package_scope in properties.PASTA_WHITELIST:
-                        msg = 'Enqueue: {} - {} - {}'.format(package.text,
-                                                             date.text, method.text)
-                        logger.warning(msg)
-                        qm.enqueue(event=event)
-                    else:
-                        logger.info('Package {} out of scope'.format(package.text))
+                    logger.info('Package {} out of scope'.format(package.text))
 
-        if toDate is None:  # we don't want to update the last_query_date while we're bootstrapping
-            adapter_utilities.save_last_query_date(query_date)
-
-    except AdapterRequestFailureException as e:
-        # we return without saving a new last_query_date so we can try again on the next pass
-        pass
 
 
 def main():
@@ -127,21 +119,28 @@ def main():
         lock.acquire()
         logger.warning('Lock file {} acquired'.format(lock.lock_file))
 
-    url = properties.PASTA_BASE_URL + '/changes/eml?'
+    url = properties.PASTA_BASE_URL + 'changes/eml?'
     qm = QueueManager()
 
     fromDate = qm.get_last_datetime()
     logger.info(f'"fromDate" from QueueManager: {fromDate}')
 
-    if fromDate is None:
+    if fromDate is None: # Empty adapter_queue database
         bootstrap(url=url)
     else:
-        # # if we've got a last_query_date, we'll use that instead of the date of the last queue entry
-        # last_query_date = adapter_utilities.get_last_query_date()
-        # if last_query_date is not None:
-        #     fromDate = last_query_date
-        #     logger.info(f'"fromDate" from adapter_utilities: {fromDate}')
-        parse(url=url, fromDate=fromDate, scope=properties.SCOPE)
+        fromDate = pendulum.instance(dt=fromDate, tz='US/Mountain')
+        # if we've got a last_query_date, we'll use that instead of the date of the last queue entry
+        last_query_date = adapter_utilities.get_last_query_date()
+        if last_query_date is not None:
+            fromDate = last_query_date.in_tz('US/Mountain')
+            logger.info(f'"fromDate" from adapter_utilities: {fromDate}')
+
+        try:
+            parse(url=url, fromDate=fromDate, scope=properties.SCOPE)
+            query_date = pendulum.now(tz='UTC')
+            adapter_utilities.save_last_query_date(query_date)
+        except AdapterRequestFailureException as e:
+            logger.error(e)
 
     lock.release()
     logger.warning('Lock file {} released'.format(lock.lock_file))
